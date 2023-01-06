5cb9abdca533838dd0b22f1d0cd2233d024d1379
jonathan-crepeau <jon.crepeau@gmail.com>
2023-01-06 15:42:18 -0800

    literal types exercises complete

--- everyday-types.ts
+++ everyday-types.ts
@@ -111,4 +111,56 @@ const printTheCoordinates = (pt: thePoint) => {
     console.log(`The coordinate's y value is ${pt.y}.`);
 }
 
-printTheCoordinates({x: 200, y: 4002});
+// printTheCoordinates({x: 200, y: 4002});
+
+// NOTE Just like when we used a type alias above, the example works just as if we had used an anonymous object type. TypeScript is only concerned with the struture of the value we passed to printTheCoordinates -- it only cares that it has the expected propreties. Being concerned only with the structure and capabilities of types is why we call TS a 'structurally typed' type system.
+
+
+// SECTION Differences Between Type Aliases and Interfaces
+
+// NOTE (Youtube - https://www.youtube.com/watch?v=crjIq7LEAYw&t=143s) By and large, it doesn't matter too much which one you choose. But as TS has matured over the years, the feature sets for both interfaces and type have gotten more and more similar. He prefers interfaces when possible -- interfaces are distinctly built to be used with objects. He uses type for functions.
+
+// NOTE Type aliases and interfaces are very similar, and in many cases you can choose between them freely. Almost all features of an interface are available in type, with the key distinction being that a type cannot be re-opened to add new properties vs. an interface which is always extendable.
+
+
+// SECTION Type Assertions
+// NOTE - See Google Drive ("[Handbook] - Everyday Types") for for info.
+
+
+// SECTION Literal Types
+
+// NOTE Both 'var' and 'let' allow for changing the values that are held inside the variables.
+let changingString = "This that pink venom.";
+changingString = "Get 'em, get 'em, get 'em.";
+
+// NOTE - hover (below) with mouse:
+changingString;
+
+// NOTE 'Const' does not allow for changing the value in the variable, and TS reflects this.
+
+const constantString = "How you like that.";
+
+// NOTE - hover (below) with mouse:
+constantString;
+
+// NOTE By combing literals into unions, you can express a much more useful concept -- for example, functions that only accept a certain set of known values:
+
+function printText(s: string, alignment: 'left' | 'right' | 'center') {
+    // some code block here
+}
+
+// printText('This that pink venom', 'left');
+// printText('This that pink venom', 'offcenter');
+
+
+// NOTE Numeric literal types work the same way:
+
+function compare(a: string, b: string): -1 | 0 | 1 {
+   return a === b ? 0 : a > b ? 1 : -1;
+}
+// NOTE If a === b, return zero (0) else(:) if that condition is not met, if a > b, return one (1) if true/truthy or negative 1 (-1) if that condition is false/falsey.
+
+// console.log(compare('Jonathan', 'Luna'))
+// console.log(compare('aaah', 'New Hampshire'))
+// console.log(compare('california', 'California'));
+// console.log(compare('California', 'California'));
